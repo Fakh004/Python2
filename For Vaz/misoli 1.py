31fc0963e308a79559f9daa14248f0a2e7d9eede
fakh <fakhridin008@gmail.com>
2025-06-12 18:51:35 +0300

    misoli 1

--- For Vaz/misoli 1.py
+++ For Vaz/misoli 1.py
@@ -1,5 +1,13 @@
-n = int(input())
+# n = int(input())
+# for i in range(1,n+1):
+#     a=' '*(n-i)        
+#     z='*'*(2*i-1) 
+#     print(a+z)
+
+n=int(input())
+k=n-1
+l=1
 for i in range(1,n+1):
-    a=' '*(n-i)        
-    z='*'*(2*i-1) 
-    print(a+z)
+    print(' '*k,l*'*')
+    k-=1
+    k+=2    
